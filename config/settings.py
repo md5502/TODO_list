cd7f08f8ca9bd0aef526ed07b1c68491af7b0698
mohammad <m.baniasadi.d@gmail.com>
2023-01-11 17:57:09 +0330

    add todolist and api and restframework to INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rdpartys
+    'rest_framework',
+
+    # local
+    'todolist',
+    'api'
 ]
 
 MIDDLEWARE = [
