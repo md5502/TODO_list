77e657512e5528cefc197a071f2405a42539f2a7
mohammad <m.baniasadi.d@gmail.com>
2023-01-12 14:51:42 +0330

    creating views for work and goal models

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import GoalSerializer, WorkSerializer
+from todolist.models import Work, Goal
 # Create your views here.
+
+class WorkViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing and editing accounts.
+    """
+    queryset = Work.objects.all()
+    serializer_class = WorkSerializer
+
+class GoalViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing and editing accounts.
+    """
+    queryset = Goal.objects.all()
+    serializer_class = GoalSerializer
