5f1dd2a0776057513399aa8eee0624b2f9cdf331
mohammad <m.baniasadi.d@gmail.com>
2023-01-12 14:52:24 +0330

    adding djoser and swagger to project

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rdpartys
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'drf_yasg',
+
 
     # local
     'todolist',
@@ -127,3 +131,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
